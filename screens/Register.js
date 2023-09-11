9f92d9c7554d3d2193252a0f0ae1aade75f22e4d
Shakith Nimsara <nimsara.shakith@gmail.com>
2023-09-11 16:01:33 +0530

    Registration Emplty Validation Check

--- screens/Register.js
+++ screens/Register.js
@@ -10,11 +10,11 @@ import {
   View,
 } from "react-native";
 import { useFonts } from "expo-font";
-import DropDownPicker from 'react-native-dropdown-picker';
+import DropDownPicker from "react-native-dropdown-picker";
 import { useState } from "react";
 import { ErrorUi } from "../components/Error";
 
-export function RegisterUI({navigation}) {
+export function RegisterUI({ navigation }) {
   const [fontsLoaded] = useFonts({
     "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
     "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.ttf"),
@@ -23,21 +23,22 @@ export function RegisterUI({navigation}) {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    {label: 'Employee', value: 'employee'},
-    {label: 'Student', value: 'student'}
+    { label: "Employee", value: "employee" },
+    { label: "Student", value: "student" },
   ]);
 
-  const [fname,setFname] = useState("");
-  const [lname,setLname] = useState("");
-  const [mobile,setMobile] = useState("");
-  const [password,setPassword] = useState("");
-  const [cpassword,setCPassword] = useState("");
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCPassword] = useState("");
 
-  const [error_1,setError_1] = useState(null);
-  const [error_2,setError_2] = useState(null);
-  const [error_3,setError_3] = useState(null);
-  const [error_4,setError_4] = useState(null);
-  const [error_5,setError_5] = useState(null);
+  const [error_1, setError_1] = useState(null);
+  const [error_2, setError_2] = useState(null);
+  const [error_3, setError_3] = useState(null);
+  const [error_4, setError_4] = useState(null);
+  const [error_5, setError_5] = useState(null);
+  const [error_6, setError_6] = useState(null);
 
   if (fontsLoaded) {
     return (
@@ -47,61 +48,95 @@ export function RegisterUI({navigation}) {
           <Image source={require("../assets/images/icon.png")} />
         </View>
         <View style={styles.contentView}>
-          <TextInput style={styles.input} placeholder="First Name" onChangeText={setFname} onChange={()=> setError_1(null)} />
-          <ErrorUi error={error_1}/>
-          <TextInput style={styles.input} placeholder="Last Name" onChangeText={setLname}/>
+          <TextInput
+            style={styles.input}
+            placeholder="First Name"
+            onChangeText={setFname}
+            onChange={() => setError_1(null)}
+          />
+          <ErrorUi error={error_1} />
+          <TextInput
+            style={styles.input}
+            placeholder="Last Name"
+            onChangeText={setLname}
+            onChange={() => setError_2(null)}
+          />
+          <ErrorUi error={error_2} />
           <TextInput
             style={styles.input}
             placeholder="Mobile Number"
             maxLength={10}
             inputMode="numeric"
             onChangeText={setMobile}
+            onChange={() => setError_3(null)}
           />
-          <DropDownPicker 
-                open={open}
-                value={value}
-                items={items}
-                setOpen={setOpen}
-                setValue={setValue}
-                setItems={setItems}
-                disableBorderRadius={true}
-                textStyle={{fontSize: 18,fontFamily: "Montserrat-Regular",letterSpacing:1}}
-                containerStyle={{width:310,margin:5}}
-                placeholder="Select User Type"
-                />
-
+          <ErrorUi error={error_3} />
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            disableBorderRadius={false}
+            textStyle={{
+              fontSize: 18,
+              fontFamily: "Montserrat-Regular",
+              letterSpacing: 1,
+            }}
+            containerStyle={{ width: 310, margin: 5 }}
+            placeholder="Select User Type"
+            onChangeValue={() => setError_4(null)}
+          />
+          <ErrorUi error={error_4} />
           <TextInput
             style={styles.input}
             placeholder="Password"
             secureTextEntry={true}
             onChangeText={setPassword}
+            onChange={() => setError_5(null)}
           />
+          <ErrorUi error={error_5} />
           <TextInput
             style={styles.input}
             placeholder="Confirm Password"
             secureTextEntry={true}
             onChangeText={setCPassword}
+            onChange={() => setError_6(null)}
           />
-          
-          <Pressable onPress={async ()=>{
-           const response = await fetch("http://192.168.43.9/MyNotes/register.php",
-           {
-            method:"POST",
-            body:JSON.stringify(
-              {
-                "fname": fname,
-                "lname": lname,
-                "mobile": mobile,
-                "type": value,
-                "password": password,
-                "c_password": cpassword,
-              }
-            )
-           });
-           const responseText = await response.text();
-           if(responseText == "error_1")
-              setError_1("Please enter first name");
-          }}>
+          <ErrorUi error={error_6} />
+
+          <Pressable
+            onPress={async () => {
+              const response = await fetch(
+                "http://192.168.43.9/MyNotes/register.php",
+                {
+                  method: "POST",
+                  body: JSON.stringify({
+                    fname: fname,
+                    lname: lname,
+                    mobile: mobile,
+                    type: value,
+                    password: password,
+                    c_password: cpassword,
+                  }),
+                }
+              );
+              const responseText = await response.text();
+              if (responseText == "error_1")
+                setError_1("Please enter first name");
+              else if (responseText == "error_2")
+                setError_2("Please enter last name");
+              else if (responseText == "error_3")
+                setError_3("Please enter mobile");
+              else if (responseText == "error_4")
+                setError_4("Please select user type");
+              else if (responseText == "error_5")
+                setError_5("Please enter password");
+              else if (responseText == "error_6")
+                setError_6("Please enter confirm password");
+            }}
+          >
             <View style={styles.loginButton}>
               <Text style={styles.text}>Create New Account</Text>
             </View>
@@ -116,8 +151,6 @@ export function RegisterUI({navigation}) {
     );
   }
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
